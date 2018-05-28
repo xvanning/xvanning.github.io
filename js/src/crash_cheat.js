784ecc882408128e4e383957490ca61c5e87e445
xvanning <130809016@qq.com>
2018-05-28 17:09:17 +0800

    Site updated: 2018-05-28 17:09:15

--- js/src/crash_cheat.js
+++ js/src/crash_cheat.js
@@ -4,12 +4,12 @@
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
          $('[rel="icon"]').attr('href', "/img/TEP.ico");
-         document.title = '╭(°A°`)╮ 爱我别走 ~~~';
+         document.title = '你快回来 ~~~';
          clearTimeout(titleTime);
      }
      else {
          $('[rel="icon"]').attr('href', "/favicon.ico");
-         document.title = '(ฅ>ω<*ฅ) 你又回来啦 ~~~' + OriginTitle;
+         document.title = '你别离开 ~~~' + OriginTitle;
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
          }, 2000);
